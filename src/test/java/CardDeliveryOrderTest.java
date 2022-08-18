a041c1b8f7df0a7b6de4f952b7ef830d3d66eb62
Daniil Peresumkin <danilka5984ka@yandex.ru>
2022-08-18 11:54:34 +0700

    test refinement

--- src/test/java/CardDeliveryOrderTest.java
+++ src/test/java/CardDeliveryOrderTest.java
@@ -1,12 +1,16 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.DateChange;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryOrderTest extends DateChange {
+    String planningDate = generateDate(15);
 
     @Test
     void testValidValues() {
@@ -15,11 +19,14 @@ public class CardDeliveryOrderTest extends DateChange {
         $("[data-test-id='city'] input").val("Москва");
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").val(dateChangeDay());
+        $("[data-test-id='date'] input").val(planningDate);
         $("[data-test-id='name'] input").val("Иванов-Петров Иван");
         $("[data-test-id='phone'] input").val("+79879879879");
         $("[data-test-id='agreement'] .checkbox__box").click();
         $("[type='button'] .button__text").click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
     @Test
@@ -30,11 +37,14 @@ public class CardDeliveryOrderTest extends DateChange {
         $$("[class='popup__container'] span").findBy(text("Новосибирск")).click();
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").val(dateChangeDay());
+        $("[data-test-id='date'] input").val(planningDate);
         $("[data-test-id='name'] input").val("Иванов-Петров Иван");
         $("[data-test-id='phone'] input").val("+79879879879");
         $("[data-test-id='agreement'] .checkbox__box").click();
         $("[type='button'] .button__text").click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
     @Test
@@ -45,10 +55,13 @@ public class CardDeliveryOrderTest extends DateChange {
         $$("[class='popup__container'] span").findBy(text("Новосибирск")).click();
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").val(dateChangeWeek());
+        $("[data-test-id='date'] input").val(planningDate);
         $("[data-test-id='name'] input").val("Иванов-Петров Иван");
         $("[data-test-id='phone'] input").val("+79879879879");
         $("[data-test-id='agreement'] .checkbox__box").click();
         $("[type='button'] .button__text").click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
--- src/test/java/ru/netology/DateChange.java
+++ src/test/java/ru/netology/DateChange.java
@@ -1,26 +1,11 @@
 package ru.netology;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class DateChange {
-    public String dateChangeDay() {
-        Calendar calendar = new GregorianCalendar();
-        calendar.roll(Calendar.DAY_OF_MONTH, 3);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-        String text = sdf.format(calendar.getTime());
 
-        return text;
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
-
-    public String dateChangeWeek() {
-        Calendar calendar = new GregorianCalendar();
-        calendar.roll(Calendar.DAY_OF_MONTH, 7);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-        String text = sdf.format(calendar.getTime());
-
-        return text;
-    }
-
 }
